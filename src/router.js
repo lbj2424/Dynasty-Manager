6681835709c4fe0c962f09d5dcbdc53f89326087
lbj2424 <166747255+lbj2424@users.noreply.github.com>
2026-02-06 13:07:05 -0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,10 +4,14 @@ import { ScoutingScreen } from "./ui/screens/scouting.js";
 import { PlayoffsScreen } from "./ui/screens/playoffs.js";
 import { FreeAgencyScreen } from "./ui/screens/freeAgency.js";
 import { DraftScreen } from "./ui/screens/draft.js";
+import { TeamScreen } from "./ui/screens/team.js";
+import { StandingsScreen } from "./ui/screens/standings.js";
 
 export const routes = {
   "/": HomeScreen,
   "/dashboard": DashboardScreen,
+  "/team": TeamScreen,
+  "/standings": StandingsScreen,
   "/scouting": ScoutingScreen,
   "/playoffs": PlayoffsScreen,
   "/free-agency": FreeAgencyScreen,
